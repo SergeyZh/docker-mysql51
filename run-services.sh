ab4ce6fe1c1ef28f414171bbdde51c4e3c005b28
Sergey Zhukov <sergey@jetbrains.com>
2014-10-08 00:12:48 +0400

    Implemented graceful shutdown of mysql

--- run-services.sh
+++ run-services.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
-trap "killall mysqld ; killall tail; exit 0" SIGINT SIGTERM SIGHUP
+shutdown(){
+    kill `cat ${MYSQL_BASE}/mysql.pid`
+    while [[ -f ${MYSQL_BASE}/mysql.pid ]]
+    do
+	PROGRESS="-"${PROGRESS}
+	echo ${PROGRESS}
+	sleep 1
+    done
+    killall tail
+    exit 0
+}
+
+trap shutdown SIGINT SIGTERM SIGHUP
 
 if [[ ! -f /mnt/mysql/etc/my.cnf ]] ; then
     cp /mysql/support-files/my-small.cnf /mnt/mysql/etc/my.cnf
 fi
 
-mysqld --defaults-file=/mnt/mysql/etc/my.cnf --basedir=${MYSQL_BASE} --datadir=${MYSQL_DATA}  --user=mysql $1 &
+mysqld --defaults-file=/mnt/mysql/etc/my.cnf --basedir=${MYSQL_BASE} --datadir=${MYSQL_DATA} \
+       --user=mysql --pid_file=${MYSQL_BASE}/mysql.pid $1 &
 
 touch /var/log/container.log
 tail -F /var/log/container.log &
